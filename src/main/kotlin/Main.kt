c491042e77e3bf6a9dca6f244504c385a69c752d
dinmoy <imhyojin614@gmail.com>
2023-03-30 19:18:46 +0900

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+fun main() {
+    println("Hello World!")
+    var var1=10
+    var var2=10.1f
+    var var3=10.2
+    var var4='안'
+    var var5="안드로이드"
+
+    println(var1)
+    println(var2)
+    println(var3)
+    println(var4)
+    println(var5)
+
+    var myVar=100
+    myVar=200
+    
+    var myVal=200
+    myVal=300 //오류
+
+    var a="100".toInt()
+    var b="100.123".toDouble()
+
+    println(a)
+    println(b)
+
+
+}
