d7adb148814a1c72469193dd0d53fc3da8b5f122
dinmoy <imhyojin614@gmail.com>
2023-03-30 20:01:28 +0900

    일차원 배열

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,8 @@
 fun main() {
-    //when문: 여러가지 경우에 따라 어떤 작업을 할지를 결정
-    //when은 switch~case 와 비슷하지만 break를 쓰지 않음
-    //범위로 처리가능: in 키워드 사용
-    var count:Int=90
-    if(count>=90){
-        println("if문:합격(장학생)")
-    }else if(count>=60){
-        println("if문:합격")
-    }else{
-        println("if문:불합격")
-    }
-    
-    var jumsu: Int=(count/10)*10
-    when(jumsu){
-        in 90..100 ->  println("when문:합격(장학생)")
-        in 60..89 -> println("when문:합격")
-        else -> println("when문:불합격")
-    }
+   //일차원 배열 선언 형식 : var 배열명=Array<데이터 형식>(개수,{초기값})/var 배열명=Array<데이터 형식>(개수) {초기값}
+    var one=Array<Int>(4,{0})
+    one[0]=10
+    one[3]=20
+    println(one[0])
+    println(one[3])
 }
