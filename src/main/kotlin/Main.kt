685059639e077ac8bf672b4e455cabab1d3ee527
dinmoy <imhyojin614@gmail.com>
2023-03-30 20:07:41 +0900

    이차원 배열

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,8 @@
 fun main() {
-   //일차원 배열 선언 형식 : var 배열명=Array<데이터 형식>(개수,{초기값})/var 배열명=Array<데이터 형식>(개수) {초기값}
-    var one=Array<Int>(4,{0})
-    one[0]=10
-    one[3]=20
-    println(one[0])
-    println(one[3])
+   //이차원 배열 선언 형식: var 배열명=Array<배열 데이터 형식>(행 개수,{배열 데이터 형식(열 개수)})/var 배열명=Array<데이터형식>(개수){초깃값}
+    var two=Array<IntArray>(3,{IntArray(4)})
+    two[0][0]=100
+    two[2][3]=200
+    println(two[0][0])
+    println(two[2][3])
 }
