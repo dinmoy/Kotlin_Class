7e03750d8186a9dfdcad7cebf329e9cbd4c76d5a
dinmoy <imhyojin614@gmail.com>
2023-04-04 19:25:39 +0900

    ArrayList 예제

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,13 @@ fun main() {
     var one=ArrayList<Int>(4)
     one.add(10)
     one.add(20)
-    var sum=one.get(0)+one.get(1)
+    one.add(30)
+    one.add(40)
+    one.add(50)
+    var sum=0
+    for(num in one){
+        sum+=num
+    }
+    println(sum)
     //첫번째 값 + 두번째 값
 }
