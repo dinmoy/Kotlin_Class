2177267a1fa2857eb59dd8fa5cbe19e8edc14778
dinmoy <imhyojin614@gmail.com>
2023-04-04 18:55:25 +0900

    반복문을 이용한 이차원 배열 출력

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,5 +3,16 @@ fun main() {
     one[0]=10
     one[3]=20
     for(i in one.indices)
-        println(one[i])
+       println(one[i])
+
+    var two=Array<IntArray>(3,{IntArray(4,{2})})
+    //3행 4열의 2차원 배열
+    //row의 index: 0~2
+    //col의 index: 0~3
+    two[1][2]=100
+    two[2][3]=200
+
+    for(i in two.indices)
+        for(j in two[i].indices)
+            println(two[i][j])
 }
