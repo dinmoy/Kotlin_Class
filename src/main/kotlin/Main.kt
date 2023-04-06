0f649041b5c30d4c9f821e46ac1bd7618e4c83ed
dinmoy <imhyojin614@gmail.com>
2023-04-06 19:25:09 +0900

    코틀린 main클래스에 생성자(constructor) 생성

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,4 @@
 fun main() {
-    /*var Car1:Car=Car()
-    Car1.color="빨강"
-    Car1.speed=0
-
-    var Car2:Car=Car()
-    Car2.color="파랑"
-    Car2.speed=0
-
-    var Car3:Car=Car()
-    Car3.color="초록"
-    Car3.speed=0
-
-    Car1.upSpeed(50)
-    println("자동차1의 색상은 "+Car1.color+" 이며, 속도는 "+Car1.speed+" km입니다")
-
-    Car2.upSpeed(20)
-    println("자동차2의 색상은 "+Car2.color+" 이며, 속도는 "+Car2.speed+" km입니다")
-
-    Car3.upSpeed(250)
-    println("자동차3의 색상은 "+Car3.color+" 이며, 속도는 "+Car3.speed+" km입니다")*/
-
     var Car1:Car=Car("빨강",0)
     var Car2:Car=Car("파랑",0)
     var Car3:Car=Car("초록",0)
@@ -32,6 +11,12 @@ class Car{
     constructor(color:String,speed:Int){
         this.color=color
         this.speed=speed
+    }
+    constructor(speed:Int){
+        this.speed=speed
+    }
+    constructor(){
+
     }
     fun upSpeed(value: Int){
         if(speed+value>=200)
