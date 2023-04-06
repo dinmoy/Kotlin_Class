40395ec5c8094a3a39fb88090ddd110e326d28e2
dinmoy <imhyojin614@gmail.com>
2023-04-06 19:27:26 +0900

    정적 구성요소 추가

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,6 +8,15 @@ class Car{
     var color:String=""
     var speed:Int=0
 
+    companion object {
+        var carCount:Int=0
+        const val MAXSPEED: Int = 200
+        const val MINSPEED: Int=0
+        //메서드명 뒤에 return타입 쓰기
+        fun currentCarCount(): Int{
+            return carCount
+        }
+    }
     constructor(color:String,speed:Int){
         this.color=color
         this.speed=speed
