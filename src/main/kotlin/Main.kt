da3c3dd925a81b1e6b118ba428d529c949af4d1b
dinmoy <imhyojin614@gmail.com>
2023-04-06 19:33:37 +0900

    코틀린 정적 필드,메소드

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,13 @@ fun main() {
     var Car2:Car=Car("파랑",0)
     var Car3:Car=Car("초록",0)
 
+    println("생산된 차의 대수(정적 필드) ==>"+Car.carCountzh)
+    println("생산된 차의대수(정적 메소드 ==>"+Car.currentCarCount())
+    println("차의 최고 제한속도==>"+Car.MAXSPEED)
+    println("PI의 값 -->"+Math.PI)
+    println("3의 5제곱==>"+Math.pow(3.0,5.0))
+
+
 }
 class Car{
     var color:String=""
